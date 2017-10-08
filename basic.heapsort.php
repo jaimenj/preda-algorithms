7d0164734fb4cd45554bbcc17996da207f9be4a7
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2017-10-08 23:09:15 +0200

    Encapsulating objects for Heapsort, testing if objects are cloned inside heaps or not.

--- basic.heapsort.php
+++ basic.heapsort.php
@@ -1,5 +1,6 @@
 <?php
-include('dataStructure.heaps.php');
+
+include 'dataStructure.heaps.php';
 
 define('NUMBER_OF_NODES', 10);
 
@@ -8,22 +9,26 @@ $maxheap = new MyTNodesMaxHeap();
 
 for ($i = 1; $i <= NUMBER_OF_NODES; ++$i) {
     $obj = new TNodo();
-    $obj->cost = rand(1, 10);
-    $obj->name = 'Task '.$i;
+    $obj->setCost(rand(1, 10));
+    $obj->setString('Task '.$i);
     $minheap->insert($obj);
     $maxheap->insert($obj);
-    echo 'Inserted << '.$obj->name.' (cost '.$obj->cost.')'.PHP_EOL;
+    echo 'Inserted << '.$obj.' (cost '.$obj->getCost().')'.PHP_EOL;
+
+    if ($i == 5) {
+        $obj->setString('String5 changed!');
+    }
 }
 
 echo 'Inserted '.NUMBER_OF_NODES.' nodes.'.PHP_EOL;
 
 echo 'Sorting with minheap:'.PHP_EOL;
-for ($i=0; $i < NUMBER_OF_NODES; $i++) {
+for ($i = 0; $i < NUMBER_OF_NODES; ++$i) {
     $obj = $minheap->extract();
-    echo 'Extracted >> '.$obj->name.' (cost '.$obj->cost.')'.PHP_EOL;
+    echo 'Extracted >> '.$obj.' (cost '.$obj->getCost().')'.PHP_EOL;
 }
 echo 'Sorting with maxheap:'.PHP_EOL;
-for ($i=0; $i < NUMBER_OF_NODES; $i++) {
+for ($i = 0; $i < NUMBER_OF_NODES; ++$i) {
     $obj = $maxheap->extract();
-    echo 'Extracted >> '.$obj->name.' (cost '.$obj->cost.')'.PHP_EOL;
+    echo 'Extracted >> '.$obj.' (cost '.$obj->getCost().')'.PHP_EOL;
 }
--- dataStructure.heaps.php
+++ dataStructure.heaps.php
@@ -2,17 +2,37 @@
 
 class TNodo
 {
-    public $cost;
-    public $name;
+    private $cost;
+    private $string; // The object stored is a simple String.
+
+    public function setCost($cost)
+    {
+        $this->cost = $cost;
+    }
+
+    public function getCost()
+    {
+        return $this->cost;
+    }
+
+    public function setString($string)
+    {
+        $this->string = $string;
+    }
+
+    public function __toString()
+    {
+        return $this->string;
+    }
 }
 
 class MyTNodesMinHeap extends SplHeap
 {
     public function compare($a, $b)
     {
-        if ($a->cost < $b->cost) {
+        if ($a->getCost() < $b->getCost()) {
             return 1; // a is better
-        } elseif ($a->cost > $b->cost) {
+        } elseif ($a->getCost() > $b->getCost()) {
             return -1; // b is better
         } else {
             return 0;
@@ -24,9 +44,9 @@ class MyTNodesMaxHeap extends SplHeap
 {
     public function compare($a, $b)
     {
-        if ($a->cost > $b->cost) {
+        if ($a->getCost() > $b->getCost()) {
             return 1; // a is better
-        } elseif ($a->cost < $b->cost) {
+        } elseif ($a->getCost() < $b->getCost()) {
             return -1; // b is better
         } else {
             return 0;
